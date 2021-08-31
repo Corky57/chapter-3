37bd19b4215bda1efff00962e02d43ce720d8467
Oliver Anthony Corcoran <04OLCOA@skola.lidkoping.se>
2021-08-31 08:40:14 +0200

    change name

--- chapter 3/Program.cs
+++ chapter 3/Program.cs
@@ -6,7 +6,7 @@ namespace chapter_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Oliver!");
         }
     }
 }
